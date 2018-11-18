88e004acbfe4c960fc0dff4afa0fc10d6992f595
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-11-18 17:41:55 +0900

    add scene_state_intro(pass)
    add scene_state_intro(pass)

--- bubble_bobble.py
+++ bubble_bobble.py
@@ -1,9 +1,11 @@
-import game_framework
 import pico2d
+import game_framework
+
+import scene_state_intro
 
 game_framework.windowScale = 4
 windowScale = game_framework.windowScale
 
 pico2d.open_canvas(320 * windowScale, 240 * windowScale)
-game_framework.run(game_framework.TestGameState('test'))
+game_framework.run(scene_state_intro)
 pico2d.close_canvas()
---
+++ scene_state_intro.py
@@ -0,0 +1,28 @@
+from pico2d import *
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    update_canvas()
+
+
+def handle_events():
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
