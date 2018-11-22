384ad2dbb3c63d6dbb27a1b8c00f88ee15260cd9
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-11-23 01:33:08 +0900

    add dragons at selection scene
    add dragons at selection scene

--- scene_state_selection.py
+++ scene_state_selection.py
@@ -13,7 +13,7 @@ back_scroll = None
 def enter():
     global image_back, image_player, back_scroll
     image_back = load_image('resources\\sprites\\lobby_state\\background.png')
-    image_player = load_image('resources\\sprites\\lobby_state\\player.png')
+    image_player = load_image('resources\\sprites\\selection_state\\selection.png')
     back_scroll = 0.0
 
 
@@ -42,22 +42,34 @@ def draw():
     image_back.draw_to_origin((-back_scroll + app.width) * app.scale, (back_scroll - app.height / 7 * 12) * app.scale,
                               app.width * app.scale, app.height * app.scale)
 
+    image_player.clip_draw_to_origin(0, 0, 26, 22,
+                                     16 * app.scale, (app.height / 2 - 35 + 16) * app.scale,
+                                     26 * app.scale, 22 * app.scale)
     font.draw('THE MOST', 8, app.height / 2 - 32 - 0, font.WHITE)
     font.draw('AVERAGE', 8, app.height / 2 - 32 - 8, font.WHITE)
     font.draw('ONE OF', 8, app.height / 2 - 32 - 16, font.WHITE)
     font.draw('THE BUNCH', 8, app.height / 2 - 32 - 24, font.WHITE)
 
+    image_player.clip_draw_to_origin(26, 0, 26, 22,
+                                     (app.width * 0.5 - 64) * app.scale, (app.height / 2 - 35 + 16) * app.scale,
+                                     26 * app.scale, 22 * app.scale)
     font.draw('THE', app.width * 0.5 - 64, app.height / 2 - 32 - 0, font.WHITE)
     font.draw('FASTEST', app.width * 0.5 - 64, app.height / 2 - 32 - 8, font.WHITE)
     font.draw('ONE OF', app.width * 0.5 - 64, app.height / 2 - 32 - 16, font.WHITE)
     font.draw('THE FOUR', app.width * 0.5 - 64, app.height / 2 - 32 - 24, font.WHITE)
 
+    image_player.clip_draw_to_origin(52, 0, 26, 22,
+                                     (app.width * 0.5 + 16) * app.scale, (app.height / 2 - 35 + 16) * app.scale,
+                                     26 * app.scale, 22 * app.scale)
     font.draw('THE ONE', app.width * 0.5 + 8, app.height / 2 - 32 - 0, font.WHITE)
     font.draw('WHOSE', app.width * 0.5 + 8, app.height / 2 - 32 - 8, font.WHITE)
     font.draw('BUBBLES', app.width * 0.5 + 8, app.height / 2 - 32 - 16, font.WHITE)
     font.draw('FLOAT THE', app.width * 0.5 + 8, app.height / 2 - 32 - 24, font.WHITE)
     font.draw('FURTHEST', app.width * 0.5 + 8, app.height / 2 - 32 - 32, font.WHITE)
 
+    image_player.clip_draw_to_origin(78, 0, 26, 22,
+                                     (app.width - 64) * app.scale, (app.height / 2 - 35 + 16) * app.scale,
+                                     26 * app.scale, 22 * app.scale)
     font.draw('THE ONE', app.width - 64, app.height / 2 - 32 - 0, font.WHITE)
     font.draw('WHOSE', app.width - 64, app.height / 2 - 32 - 8, font.WHITE)
     font.draw('BUBBLES', app.width - 64, app.height / 2 - 32 - 16, font.WHITE)
