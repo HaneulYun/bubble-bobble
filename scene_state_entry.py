ed9d6497acd89ef8afeacb5180b71f06aa691891
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-11-23 19:56:32 +0900

    add game scene
    add game scene

--- scene_state_entry.py
+++ scene_state_entry.py
@@ -3,6 +3,7 @@ import game_framework
 
 import app
 import font
+import scene_state_game
 
 name = 'scene_state_entry'
 image_back = None
@@ -40,6 +41,9 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_a):
+                game_framework.change_state(scene_state_game)
 
 
 def pause():
---
+++ scene_state_game.py
@@ -0,0 +1,41 @@
+from pico2d import *
+import game_framework
+
+import app
+import font
+
+name = 'scene_state_game'
+image_back = None
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
