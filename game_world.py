a12755a2ebc8099a95a17e6d9326bd867d2a0d39
HaneulYun(Aorus) <yhnil.kr@gmail.com>
2018-11-23 19:58:49 +0900

    add game_world
    add game_world

---
+++ game_world.py
@@ -0,0 +1,31 @@
+objects = None
+
+
+def init_objects():
+    global objects
+    objects = [[], []]
+
+
+def add_object(o, layer):
+    objects[layer].append(o)
+
+
+def remove_object(o):
+    for i in range(len(objects)):
+        if o in objects[i]:
+            objects[i].remove(o)
+            del o
+            break
+
+
+def clear():
+    for o in all_objects():
+        del o
+    objects.clear()
+    init_objects()
+
+
+def all_objects():
+    for i in range(len(objects)):
+        for o in objects[i]:
+            yield o
